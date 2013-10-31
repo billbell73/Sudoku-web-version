383b57e19e9149fe050a9f481cf0a56eeeab3e34
Will Bell <willbell@gmail.com>
2013-10-31 17:00:27 +0000

    implemented functionality on / and /solution

--- sudoku.rb
+++ sudoku.rb
@@ -4,49 +4,102 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
-# class Sudoku_board
+enable :sessions
 
-    def random_sudoku
-        # we're using 9 numbers, 1 to 9, and 72 zeros as an input
-        # it's obvious there may be no clashes as all numbers are unique
-        seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
-        sudoku = Sudoku.new(seed.join)
-        # then we solve this (really hard!) sudoku
-        sudoku.solve!
-        # and give the output to the view as an array of chars
-        sudoku.to_s.chars
-        # seed
+def random_sudoku
+    seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+    sudoku = Sudoku.new(seed.join)
+    sudoku.solve!
+    sudoku.to_s.chars
+end
+
+
+def puzzle sudoku
+  puzzle = []
+  sudoku.each do |val|
+    random_number = Random.rand(0..100)
+    if random_number < 60
+      puzzle << val
+    else
+      puzzle << "0"
     end
+  end
+  puzzle
+end
 
-    # def puzzle_maker sudoku
-    #   puzzle = []
-    #   sudoku.each do |val|
-    #     puzzle << [val, val, nil].sample
-    #   end
-    #   puzzle
-    # end
+get '/' do
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] #|| session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
+  erb :index
+end
 
-    def puzzle_maker sudoku
-      puzzle = []
-      sudoku.each do |val|
-        random_number = Random.rand(0..100)
-        if random_number < 30
-          puzzle << val
-        else
-          puzzle << nil
-        end
-      end
-      puzzle
+get '/solution' do
+  @current_solution = session[:solution]
+  @solution = session[:solution]
+  @puzzle = session[:solution]
+  erb :index
+end
+
+post '/' do
+  cells = box_order_to_row_order(params["cell"])  
+  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:check_solution] = true
+  redirect to("/")
+end
+
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) {|memo, i|
+      first_box_index = i / 3 * 3
+      three_boxes = boxes[first_box_index, 3]
+      three_rows_of_three = three_boxes.map do |box| 
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3]
     end
+    memo += three_rows_of_three.flatten
+  }
+end 
 
-# end
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  session[:puzzle] = puzzle(sudoku)
+  session[:current_solution] = session[:puzzle]    
+end
 
-# new_board = Sudoku_board.new
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
+end
 
-    get '/' do
-        sudoku = random_sudoku
-        # @current_solution = new_board.random_sudoku
-        #@current_solution = puzzle_maker sudoku
-        @current_solution = random_sudoku
-      erb :index
+helpers do
+
+  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
+    zero_on_original_puzzle = puzzle_value == "0"
+    now_not_zero = current_solution_value.to_i != 0
+    different_from_solution = current_solution_value != solution_value
+
+    if solution_to_check && 
+        zero_on_original_puzzle && 
+        now_not_zero && 
+        different_from_solution
+      'incorrect'
+    elsif zero_on_original_puzzle && 
+        now_not_zero && 
+        !different_from_solution
+      'correct-guess'
+    elsif !zero_on_original_puzzle
+      'value-provided'
     end
+  end
+
+   def cell_value(value)
+    value.to_i == 0 ? '' : value
+  end
+
+end
