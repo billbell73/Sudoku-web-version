7bc81a2b0365eb9594b072d96fb88da7a3cbbca5
Will Bell <willbell@gmail.com>
2014-02-24 13:24:24 +0000

    Swapped delay for second reload to function detecting end of animation

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 	if($('.incorrect-just-entered').length) {
   	setTimeout(function(){window.location.reload();},1000);
 	}
-	if($('.animated.rotateOut').length) {
-		setTimeout(function(){window.location.reload();},1000);
-		// window.location.reload();
-	}
+	// if($('.animated.rotateOut').length) {
+	// 	//setTimeout(function(){window.location.reload();},1000);
+	// 	// window.location.reload();
+	// }
+	$(".animated.rotateOut").bind("animationend webkitAnimationEnd oAnimationEnd"  
+		+ "MSAnimationEnd", function(){ window.location.reload(); });
 })
